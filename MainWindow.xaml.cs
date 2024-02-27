ab57dd1b81a3a62b35542c1931123368ff2309c5
Balogh Tamás <balogh.tamas@mechwart.local>
2024-02-27 12:46:29 +0100

    first task done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,48 @@ namespace WPF_GAMF1
         public MainWindow()
         {
             InitializeComponent();
+            Task.Run(loadNumbers);
+        }
+        async Task loadNumbers()
+        {
+            long[] numbers = await File.ReadAllLinesAsync("./files/szamok.txt").ContinueWith(res => res.Result.Select(long.Parse).ToArray());
+            var coprime = numbers.Where(x => IsCoPrime(x, 1310438493L)).Count();
+            var anagram = numbers.Where(x => areAnagrams(x, 2354211341L)).Count();
+            
+        }
+
+        List<int> TwoDigits(long number)
+        {
+            string num = number.ToString();
+            List<int> result = new();
+            for(int i = 0; i < num.Length; i++)
+            {
+
+            }
+            return result;
+        }
+
+        bool areAnagrams(long a, long b)
+        {
+            string number_1 = a.ToString();
+            string number_2 = b.ToString();
+            var sorted_1 = number_1.OrderBy(c => c).ToArray();
+            var sorted_2 = number_2.OrderBy(c => c).ToArray();
+            return sorted_1.SequenceEqual(sorted_2);
+        }
+
+        bool IsCoPrime(long a, long b)
+        {
+            long ld, ldRem;
+            while(b > 0)
+            {
+                ld = a / b;
+                ldRem = a % b;
+                a = b;
+                b = ldRem;
+
+            }
+            return a == 1;
         }
     }
 }
