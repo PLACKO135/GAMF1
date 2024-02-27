0631b8161e56b0467ff8ac69571562d073b5ec1d
Papp Laszlo <papplaszlolevente@gmail.com>
2024-02-27 14:30:01 +0100

    48 ms and task 1 ready

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,16 +29,20 @@ namespace WPF_GAMF1
 
         private async void btn_feladat1_Click(object sender, RoutedEventArgs e)
         {
-            await loadNumbers();
+            await  loadNumbers();
         }
 
         async Task loadNumbers()
         {
+            var startTime = DateTime.Now;
             long[] numbers = await File.ReadAllLinesAsync("./files/szamok.txt").ContinueWith(res => res.Result.Select(long.Parse).ToArray());
             var coprime = numbers.Where(x => IsCoPrime(x, 1310438493L)).Count();
             var anagram = numbers.Where(x => areAnagrams(x, 2354211341L)).Count();
-            var frequentDigits = numbers.Select(x => TwoDigits(x)).GroupBy(x => x).OrderByDescending(x => x.Count()).Select(x => x.Key).ToList()[0];
-            
+            var frequentDigits = numbers.SelectMany(x => TwoDigits(x)).ToList().GroupBy(x => x).OrderByDescending(x => x.Count()).Select(x => x.Key).First();
+            var endTime = DateTime.Now;
+            var peformanceTime = endTime - startTime;
+            lb_elso.Content = $"a): {coprime}\nb): {anagram}\nc): {frequentDigits}\nidő: {peformanceTime.Milliseconds} ms";
+
         }
         //O(n) time complexity
         List<int> TwoDigits(long number)
@@ -47,7 +51,7 @@ namespace WPF_GAMF1
            List<int> result = new();
            for(int i = 0; i < num.Length -1;i++)
            {
-                result.Add(int.Parse($"{num[i] + num[i + 1]}"));
+                result.Add(int.Parse($"{num[i]}{num[i+1]}"));
            }
             return result;
         }
